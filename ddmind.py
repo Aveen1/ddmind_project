e6c6f2d07a0b2e1702dd3410b5e4caa25e1c26f2
Aveen Faheem <41651935+Aveen1@users.noreply.github.com>
2025-01-16 19:39:52 +0300

    Update ddmind.py

--- ddmind.py
+++ ddmind.py
@@ -24,7 +24,7 @@ def preprocess_data(df):
             "Remove duplicate rows": lambda df: df.drop_duplicates(),
             "Remove null values": lambda df: df.dropna(),
             "Handle date columns": lambda df: handle_date_columns(df),
-            "Standardize column names": lambda df: standardize_column_names(df),
+            #"Standardize column names": lambda df: standardize_column_names(df),
             #"Ensure 'Year' column is valid": lambda df: validate_year_column(df),
             #"Validate and correct data types": lambda df: validate_data_types(df),
             "Remove low-variance columns": lambda df: remove_low_variance_columns(df),
